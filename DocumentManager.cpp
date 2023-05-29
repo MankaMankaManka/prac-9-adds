11af1ffcf4ea3ea332fbfd2536abc85b14c64ec2
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-05-29 23:31:12 +0930

    Update DocumentManager.cpp

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,7 +1,42 @@
+#include "DocumentManager.h"
 
-
-
-void DocumentManager::addDocument(string name, int id, int license_limit){
-
+void DocumentManager::addDocument(std::string name, int id, int license_limit){
+    
+    //name
+    
+    DocumentManager* pCrawl =this;
+    
+    for (int level = 0; level < name.size(); level++) 
+    {
+        // Converts name current character into index
+        // use only 'a' through 'z' and lower case
+        int index = (int)name[level] - (int)'a'; 
+        if (pCrawl->children[index]==nullptr){
+            pCrawl->children[index] = new DocumentManager;
+        }
+        pCrawl = pCrawl->children[index];
+    }
+ 
+    // mark last node as leaf
+    pCrawl->isWordEnd = true;
+    pCrawl->docid = id;
+    pCrawl->limit =license_limit;
+    //
+    
+    
 }
 
+int DocumentManager::search(std::string name) // returns docid if name is in the document collection or 0 if the name is not in the collection
+{
+        DocumentManager* pCrawl = this;
+    for (char c : name) 
+    {
+        int ind = (int)c - (int)'a';
+        pCrawl = pCrawl->children[ind];
+    }
+    if (pCrawl->isWordEnd==true){
+        return docid;
+    }
+
+    return 0;
+}
