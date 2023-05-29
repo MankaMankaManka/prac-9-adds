48b2554e733f0d3ee989062be0baea0f40507ff2
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-05-29 23:31:32 +0930

    Update DocumentManager.h

--- DocumentManager.h
+++ DocumentManager.h
@@ -1,15 +1,34 @@
-
+#ifndef DOCUMENTMANAGER_H
+#define DOCUMENTMANAGER_H
+#include <iostream>
 
 class DocumentManager {
  private:
+ 
+DocumentManager* children[26];
+DocumentManager* integer[10];
+bool isWordEnd;
+int docid;
+int limit;
+int borrowed=0;
 
-void addDocument(string name, int id, int license_limit);
+DocumentManager(){
+    this->isWordEnd = false;
+ 
+        for (int i = 0; i < 26; i++) {
+            this->children[i] = nullptr;
+        }
+};
+
+void addDocument(std::string name, int id, int license_limit);
 
 void addPatron(int patronID);
 
-int search(string name); // returns docid if name is in the document collection or 0 if the name is not in the collection
+int search(std::string name); // returns docid if name is in the document collection or 0 if the name is not in the collection
 
 bool borrowDocument(int docid, int patronID);  // returns true if document is borrowed, false if it can not be borrowed (invalid patronid or the number of copies current borrowed has reached the license limit)
 
 void returnDocument(int docid, int patronID);
 };
+
+#endif
